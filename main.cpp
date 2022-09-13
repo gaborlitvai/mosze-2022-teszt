8aa14ec023830cc3680a3786e561d6d7c88f43c3
litvaigabor <litvaigabor@gmail.com>
2022-09-13 06:43:16 +0200

    Commented bugs

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //hibas konstans nev,
+    std::cout << '1-100 ertekek duplazasa' //string helyett karakterkent van kezelve, hianyzo pontosvessszo es sor emeles
+    for (int i = 0;) //hianyos for ciklus, hibas kezdoertek
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //hianyos for ciklus, hibas kezdoertek
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //hianyzik a kiirando ertek, a pontosvesszo es a soremeles
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i]
+        atlag += b[i] //hianyzo pontosvesszo
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
